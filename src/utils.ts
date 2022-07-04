f217dfd8d49779dfe491eb7663988c2f0c40bf4b
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-04 06:37:48 +0000

    fix styling

--- src/utils.ts
+++ src/utils.ts
@@ -17,16 +17,20 @@ export function makeUrl(href: string, pageUrl: string) {
 
 export async function getUrl(url: string, options?: https.RequestOptions) {
   return new Promise<string>((resolve, reject) => {
-    ;(url.startsWith('https://') ? https : http).get(url, { ...options }, (res) => {
-      let data = ''
-      res.setEncoding('utf8')
-      res.on('error', reject)
-      res.on('data', (chunk) => {
-        data += chunk as string
-      })
-      res.on('end', () => {
-        resolve(data)
-      })
-    })
+    ;(url.startsWith('https://') ? https : http).get(
+      url,
+      { ...options },
+      (res) => {
+        let data = ''
+        res.setEncoding('utf8')
+        res.on('error', reject)
+        res.on('data', (chunk) => {
+          data += chunk as string
+        })
+        res.on('end', () => {
+          resolve(data)
+        })
+      },
+    )
   })
 }
