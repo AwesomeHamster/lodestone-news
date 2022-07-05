eefd41b82097285496cb419826f5b511e64f1b64
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-05 00:52:25 +0000

    recover test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,34 +1,39 @@
 import https from 'https'
 import { expect } from 'chai'
 import getNews, { regions } from '../src'
-import ProxyAgent from 'proxy-agent'
+
 describe('lodestone-news', () => {
-
-  const proxy = 'http://localhost:7890'
-  const agent = new ProxyAgent(proxy)
-  describe('use agent', () => {
-    it('should accept agent', async () => {
-      const news = await getNews({
-        region: 'jp',
-        category: 'topics',
-        count: 20,
-        request: { agent }
-      })
-      expect(news).is.an('array')
-      expect(news).has.length.which.is.greaterThan(0)
-    }).timeout(0)
+  regions.forEach((region) => {
+    describe(`Locale: ${region}`, () => {
+      ;['topics', 'notices', 'maintenance', 'updates', 'status'].forEach(
+        (category) => {
+          it(`should fetch ${category}`, async () => {
+            const news = await getNews({
+              region,
+              category,
+              count: 20,
+            })
+            expect(news).is.an('array')
+            expect(news).has.length.greaterThan(0)
+            expect(news[0]).has.property('title').which.is.a('string')
+            expect(news[0]).has.property('epoch').which.is.a('number')
+            expect(news[0]).has.property('url').which.is.a('string')
+            expect(news[0]).has.property('date').which.is.a('date')
+          }).timeout(0)
+        },
+      )
+    })
   })
 
-  describe('use direct connect', () => {
-    it('not use agent', async () => {
-      const news = await getNews({
-        region: 'jp',
-        category: 'topics',
-        count: 20
-      })
-      expect(news).is.an('array')
-      expect(news).has.length.which.is.greaterThan(0)
-    }).timeout(0)
+  it('should accept agent', async () => {
+    const agent = new https.Agent({ keepAlive: true })
+    const news = await getNews({
+      region: 'jp',
+      category: 'topics',
+      count: 20,
+      request: { agent },
+    })
+    expect(news).is.an('array')
+    expect(news).has.length.which.is.greaterThan(0)
   })
-
 })
