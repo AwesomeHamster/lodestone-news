4f22b837eaa2b3d23871b1a19cc174df05253470
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-08-31 06:31:27 +0000

    test: optimise test with chai custom method overwrite

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,10 +1,31 @@
 import * as https from 'https'
 
-import { expect } from 'chai'
+import { Assertion, expect } from 'chai'
 
 import { defaultRules, getNews, LodestoneNews, regions } from '../src'
 
 const UTC_0_2022_07_19_07_59_59 = 1658217599 * 1000 // 2022-07-19 07:59:59 UTC+0
+
+const a = function (_super: typeof Assertion) {
+  return function (this: typeof Assertion, value: any, message?: string) {
+    if (value === 'news') {
+      new Assertion(this._obj).to.have.property('title').which.is.a('string')
+      new Assertion(this._obj).to.have.property('url').which.is.a('string')
+      new Assertion(this._obj).to.have.property('epoch').which.is.a('number')
+      new Assertion(this._obj).has.property('date').which.is.a('date')
+    } else {
+      _super.apply(this, [value, message])
+    }
+  }
+}
+
+function noop() {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  return function () {}
+}
+
+Assertion.overwriteChainableMethod('a', a, noop)
+Assertion.overwriteChainableMethod('an', a, noop)
 
 regions.forEach((region) => {
   describe(`Locale: ${region}`, () => {
@@ -18,10 +39,7 @@ regions.forEach((region) => {
           })
           expect(news).is.an('array')
           expect(news).has.length.greaterThan(0)
-          expect(news[0]).has.property('title').which.is.a('string')
-          expect(news[0]).has.property('epoch').which.is.a('number')
-          expect(news[0]).has.property('url').which.is.a('string')
-          expect(news[0]).has.property('date').which.is.a('date')
+          expect(news[0]).is.a('news')
         }).timeout(0)
       },
     )
@@ -51,10 +69,7 @@ it('should accept custom config', async () => {
   const news = await lodestone.getNews({ category: 'custom' })
   expect(news).is.an('array')
   expect(news.length).equals(10)
-  expect(news[0]).has.property('title').which.is.a('string')
-  expect(news[0]).has.property('epoch').which.is.a('number')
-  expect(news[0]).has.property('url').which.is.a('string')
-  expect(news[0]).has.property('date').which.is.a('date')
+  expect(news[0]).is.a('news')
 })
 
 it('should filter with after / before', async () => {
@@ -66,11 +81,8 @@ it('should filter with after / before', async () => {
     before: new Date('2022-07-20'),
   })
   expect(news).is.an('array')
-  expect(news[0]).has.property('title').which.is.a('string')
-  expect(news[0]).has.property('epoch').which.is.a('number')
-  expect(news[0]).has.property('url').which.is.a('string')
-  expect(news[0]).has.property('date').which.is.a('date')
-  expect(news.length).is.equal(2)
+  expect(news[0]).to.be.a('news')
+  expect(news).has.length(2)
   expect(news[0].date).is.lessThan(new Date('2022-07-20'))
   expect(news[news.length - 1].date).is.greaterThan(new Date(UTC_0_2022_07_19_07_59_59))
 }).timeout(0)
@@ -83,9 +95,6 @@ it('should filter with after', async () => {
     after: new Date(UTC_0_2022_07_19_07_59_59),
   })
   expect(news).is.an('array')
-  expect(news[0]).has.property('title').which.is.a('string')
-  expect(news[0]).has.property('epoch').which.is.a('number')
-  expect(news[0]).has.property('url').which.is.a('string')
-  expect(news[0]).has.property('date').which.is.a('date')
-  expect(news[news.length - 1].date).is.greaterThan(new Date(UTC_0_2022_07_19_07_59_59))
+  expect(news[0]).is.a('news')
+  expect(news.slice(-1)[0].date).is.greaterThan(new Date(UTC_0_2022_07_19_07_59_59))
 }).timeout(0)
